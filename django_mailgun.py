15718fb620f1d47f85e2560179463a26ff04a1c8
Daniel Roy Greenfeld <pydanny@gmail.com>
2016-05-03 00:48:49 -0700

    Replace missing paranthesis.

--- django_mailgun.py
+++ django_mailgun.py
@@ -89,7 +89,7 @@ class MailgunBackend(BaseEmailBackend):
                     for data in data_to_transform:
                         api_data.append((api_transformer[0], api_transformer[1](data)))
                 elif isinstance(data_to_transform, dict):
-                    for data in six.iteritems(data_to_transform:
+                    for data in six.iteritems(data_to_transform):
                         api_data.append(api_transformer(data))
                 else:
                     # we only have one value
